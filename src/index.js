db59a74731d86df73a571c04dc749b738eb81074
santosh <santoshnepal608@gmail.com>
2021-07-23 18:24:05 +0545

    Project Started

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-function BookList(){
-    return `Hello`;
-}
+function BookList() {
+    return (
+      <section>
+        <Book />      
+      </section>
+    )
+  }
+  
+  const Book = () => {
+     return (
+       <article>
+         <Image />
+         <Image />
+         <Image />
+       </article>
+     )
+  }
+  
+  const Image = () => <img src = "https://m.media-amazon.com/images/I/714PYe6ugmL._AC_UY218_.jpg" alt= '' />
 
 ReactDOM.render(<BookList />,document.getElementById('root'));
